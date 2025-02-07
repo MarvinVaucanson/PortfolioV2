c5519d8231f8955bee5c2e88ee9234a1905807fe
Baptiste <baptiste.rousselot@etu.univ-lyon1.fr>
2025-02-07 11:33:23 +0100

    PrimeVue init #1

--- portfolioV2/src/components/HelloWorld.vue
+++ portfolioV2/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Button from "primevue/button"
 
 defineProps<{ msg: string }>()
 
@@ -11,6 +12,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <Button label="douze" severity="danger"></Button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- portfolioV2/src/main.ts
+++ portfolioV2/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Nora from '@primevue/themes/nora';
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+app.use(PrimeVue, {
+    theme: {
+        preset: Nora
+    }
+});
+
