f9bfbe50c86a2a8df8428bc546e50131887435a3
Baptiste R <bapt.rousselot@gmail.com>
2025-10-05 19:59:26 +0200

    fix merge

---
+++ portfolioV2/src/components/Passions.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="">
+        <div class="MenuCatMain pb-6">
+        <div class="pl-5 py-3">
+            <h3 class="text-2xl font-bold py-3">
+                ./ Passions<span id="underscore" class="blink">_</span>
+            </h3>
+            <p class="text-xl"> 
+                Photos, Montagnes et Musiques (un peu d'informatique et de jeux vidéo aussi)
+            </p>
+        </div>
+        </div>
+        <div class='InfoCatMain pb-3 bg-red-900'>
+            <h3 class="text-xl pl-16 pt-3">// Mes photos</h3>
+            <div class="grid grid-cols-4 gap-6 px-16 py-6">
+                <div class="flex flex-col items-center">
+                    <img 
+                        src="https://pxscdn.com/public/m/_v2/784420339448408764/4f692c7f1-e20d1b/bY5wXcpwPYd9/vzlMj20hYTezV0Li2HmAcVuXt82bucIOqSIESBUf.png" 
+                        alt="Venise antifa"
+                        class="w-64 h-64 object-cover rounded-lg shadow bg-red-950"
+                    >
+                    <p class="text-center mt-2">Trip To Venise</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <img 
+                        src="https://pxscdn.com/public/m/_v2/784420339448408764/634f2a4e4-291f24/eE4stvkCX9TC/vnJTDiGQOoWPy5tT85CoRpGyYCveoRv9xR4Eo5fc.jpg" 
+                        alt="homme perché sur un toit qui regarde l'horizon"
+                        class="w-64 h-64 object-cover rounded-lg shadow"
+                    >
+                    <p class="text-center mt-2">Maître Corbeau</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <img 
+                        src="https://pxscdn.com/public/m/_v2/784420339448408764/758e75a50-ddd61a/hs2yu4M8CbwB/pz5jYzrs1Pgoadh3tfyKOMjUzU2Vjg3kHKrpILux.jpg" 
+                        alt="Guitare sur une table en bois"
+                        class="w-64 h-64 object-cover rounded-lg shadow"
+                    >
+                    <p class="text-center mt-2">Souvenir de vacances</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <img 
+                        src="https://pxscdn.com/public/m/_v2/784420339448408764/758e75a50-ddd61a/9Tz0Z9zvdX8d/Xm2vj9baJtBI4OtR0tVwumUP5OzNR6wWdvSLIVBQ.jpg" 
+                        alt="Flamme rouge manif"
+                        class="w-64 h-64 object-cover rounded-lg shadow"
+                    >
+                    <p class="text-center mt-2">Rouge et Noir</p>
+                </div>
+            </div>
+            <h3 class="text-xl pl-16 pt-3">// Vous voulez en savoir plus sur mes passions ?</h3>
+            <h3 class="text-xl pl-16 pt-3 hover:underline"><a href="https://pixelfed.social/Marvin_vcs">↪ Retrouvez moi sur PixelFed</a></h3>
+
+        </div>
+    </div>
+</template>
